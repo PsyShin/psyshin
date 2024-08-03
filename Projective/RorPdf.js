a3e2f53a0486fa9f5b77682a79e3d5814c62ce16
PsyShin <chenpatrick94@gmail.com>
2024-08-03 19:34:34 +0800

    Delete Projective directory

--- Projective/RorPdf.js
+++
@@ -1,6 +0,0 @@
-function generateRep(){
-	document.getElementById('generateRep').style.display = "none";
-	var Zscore_Ele = window.opener.document.getElementsByName("StrSum_Z");
-	var Zscore_Pdf = document.getElementsByName("Zscore");
-	for(let i=0; i<3; i++) Zscore_Pdf[i].innerHTML = Zscore_Ele[i].innerHTML;
-}
