54b200ffc51ebdacdddf580cfaed95b729050b54
PsyShin <chenpatrick94@gmail.com>
2024-08-31 22:11:09 +0800

    Add files via upload

---
+++ DSM5/DSM5.js
@@ -0,0 +1,29 @@
+function ShowChild(childname){
+	var ChildsCon = document.getElementsByClassName("Childs");
+	for(let i=0; i<ChildsCon.length; i++) ChildsCon[i].style = "display: none;";
+	document.getElementById(childname).style = "display: block;";
+}
+
+function ShowCriteria(Diagnosis){
+	var AllDiags = document.getElementsByClassName("AllDiags");
+	for(let i=0; i<AllDiags.length; i++) AllDiags[i].style = "display: none;";
+	document.getElementById(Diagnosis).style = "display: block;";
+	
+	document.getElementById('blank25').style = "display:none;";
+}
+
+function ShowCriteriaPh(Diagnosis){
+	var AllDiags = document.getElementsByClassName("AllDiags");
+	for(let i=0; i<AllDiags.length; i++) AllDiags[i].style = "display: none;";
+	document.getElementById(Diagnosis).style = "display: block;";
+	document.getElementById('DSM5List').style = "display: none;";
+
+	document.getElementById('blank25').style = "display:none;";
+}
+
+
+
+// for Phone
+function DSM5Btn(){
+	document.getElementById('DSM5List').style = "display: block;";
+}
