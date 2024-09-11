90421193fe425a4e8dfbe382eaaaa2b746b6f9fb
PsyShin <chenpatrick94@gmail.com>
2024-09-11 12:04:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 	var hamburgers = document.querySelectorAll('.hamburger');
-    var DSMopens = document.querySelectorAll('.DSMopen');
 
     hamburgers.forEach(function (hamburger) {
         hamburger.addEventListener('click', function () {
@@ -15,14 +14,16 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    DSMopens.forEach(function (DSMopen){
-        DSMopen.addEventListener('click', function(){
-            var password = ''
-            password=prompt('這裡是管制區，要跟管理員索取密碼才能放行哦！\n輸入密碼:','');
-            if (password != null) {
-                location.href= password + '/' + password + ".html";
-            }
-        })
+    document.getElementById('DSMopen').addEventListener('click', function(){
+        var password = '';
+        password=prompt('這裡是管制區，要跟管理員索取密碼才能放行哦！\n輸入密碼:','');
+        if (password != null) {
+            location.href= password + '/' + password + ".html";
+        };
+    });
+
+    document.getElementById('AboutShin').addEventListener('click', function(){
+        alert("心理小秘書是由Shin心理師於醫院爆肝之際\n又投入更多新鮮的肝孵育而來\n雖然心理小秘書剛出生\n但他會慢慢成長茁壯\n還望各位夥伴接納與關懷\n\n另外Shin心理師熱愛大海\n網頁的所有背景都是Shin親手拍攝\n而水下的照片也是由Shin潛水下去拍攝的\n但是Shin是潛水孤兒\n可以認領我去潛水 世界各地都可以 哭了");
     });
 });
 
