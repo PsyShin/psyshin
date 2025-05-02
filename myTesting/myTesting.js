40f67308fd3dd311f0a052944de21d3adee6565c
PsyShin <chenpatrick94@gmail.com>
2025-05-02 17:16:48 +0800

    Update myTesting.js

--- myTesting/myTesting.js
+++ myTesting/myTesting.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     document.getElementById('AboutShin').addEventListener('click', function(){
-        alert("心理小秘書是由Shin心理師於醫院爆肝之際\n又投入更多新鮮的肝孵育而來\n雖然心理小秘書剛出生\n但他會慢慢成長茁壯\n還望各位夥伴接納與關懷\n\n另外Shin心理師熱愛大海\n網頁的所有背景都是Shin親手拍攝\n而水下的照片也是由Shin潛水下去拍攝的\n但是Shin是潛水孤兒\n可以認領我去潛水 世界各地都可以 哭了");
+        alert("心理小秘書誕生的初衷是\n協助心理師處理繁瑣又沒有成長性的查表工作\n解放心理師的時間\n讓心理師能有更多的時間獨自升級\n\n雖然心理小秘書剛出生\n但他會慢慢成長茁壯\n還望各位夥伴接納與關懷\n\n如果有發現哪家醫院誤會了心理小秘書的初衷\n把小秘書拿來斂財\n逼迫心理師執行更多的心理衡鑑\n請務必聯繫小秘書（寄信給我們）\n我們將盡力封鎖該院IP，並說服您離職");
     });
 });
 
